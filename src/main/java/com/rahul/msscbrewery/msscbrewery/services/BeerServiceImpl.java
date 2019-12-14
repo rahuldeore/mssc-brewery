3e030b85b9adc8f98c037bfa3a7cf7e931307509
RahulD <rahul.deore@hotmail.com>
2019-12-14 18:28:56 -0500

    Added post beer resource in beer controller

--- src/main/java/com/rahul/msscbrewery/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/com/rahul/msscbrewery/msscbrewery/services/BeerServiceImpl.java
@@ -17,4 +17,13 @@ public class BeerServiceImpl implements BeerService {
                 .beerStyle("HefeWizen")
                 .build();
     }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder().id(UUID.randomUUID())
+                .beerName(beerDto.getBeerName())
+                .beerStyle(beerDto.getBeerStyle())
+                .upc(beerDto.getUpc())
+                .build();
+    }
 }
--- src/main/java/com/rahul/msscbrewery/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/rahul/msscbrewery/msscbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package com.rahul.msscbrewery.msscbrewery.web.controller;
 
 import com.rahul.msscbrewery.msscbrewery.services.BeerService;
 import com.rahul.msscbrewery.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -18,7 +16,7 @@ import java.util.UUID;
 @RestController
 public class BeerController {
 
-    public BeerService beerService;
+    private BeerService beerService;
 
     public BeerController(BeerService beerService) {
         this.beerService = beerService;
@@ -26,6 +24,14 @@ public class BeerController {
 
     @GetMapping("/beers/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
-        return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+        return new ResponseEntity(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+    @PostMapping("/beer")
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
+        // TODO add hostname in the header's location key
+        BeerDto newBeer = beerService.saveNewBeer(beerDto);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Location", "/beer/api/v1/beer/" + newBeer.getId().toString());
+        return new ResponseEntity(newBeer, httpHeaders, HttpStatus.CREATED);
     }
 }
